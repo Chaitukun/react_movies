d3d839c34d196b512bfe91772bf93cb5e7ae01f3
chaitu <srujandasari.2004@gmail.com>
2025-01-29 23:30:28 +0530

    imported img from figma design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { useState } from 'react'
+import Search from './components/Search'
 const App = () => {
+  const [searchTerm,setSearchTerm] = useState('');
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className="pattern " />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero section" />
+          <h1>
+             Find <span className='text-gradient'> Movies </span> you will enjoy without hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h1 className="text-white">{searchTerm}</h1>
+        </div>
+    </main>
   )
 }
 
